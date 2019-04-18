b518f9885b4179450452d6e4bb20c8dc6527a689
XXWolfBane <XXWolfBane@Gmail.com>
2019-04-18 07:28:09 -0400

    Update xp.js

--- commands/xp.js
+++ commands/xp.js
@@ -1,7 +1,7 @@
 module.exports.run = (bot, message, args, discord, con) => {
  let target = message.members.mentions.first() || message.guild.members.get(args[1]) || message.author.id
  
- con.query(`SELECT * FROM xp WHERE id = '${target.id}'` (err, rows) => {
+ con.query(`SELECT * FROM xp WHERE id = '${target.id}'`, (err, rows) => {
   if(err) throw err;
   
   if(!rows[0]) return message.channel.send("This person has no XP!")
