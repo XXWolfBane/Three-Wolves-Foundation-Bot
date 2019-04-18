34157619739e3295de70eb0617340089c978744c
XXWolfBane <XXWolfBane@Gmail.com>
2019-04-18 06:24:11 -0400

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,34 @@
+const Discord = require("discord.js");
+const bot = new Discord.Client();
+var prefix = ".";
+const fs = require("fs");
+
+bot.commands = new Discord.Collection()
+
+require('fs').readdir("./commands/", (err, files) => {
+  console.log("Loading commands...");
+  if (err) return console.log(`Command loading failed!`);
+  files.filter(f => f.split(".").pop() === "js").forEach((f, i) => {
+    bot.commands.set(require(`./commands/${f}`).help.name, require(`./commands/${f}`));
+  });
+});
+
+bot.on("ready", () => {
+  console.log("Alive.");
+  bot.user.setActivity("For .help", {type: "WATCHING"});
+});
+
+bot.on('message', message => {
+  let mArray = message.content.split(" ")
+  let args = mArray.slice(1)
+  let cmd = bot.commands.get(mArray[0].slice(prefix.length))
+  if (message.author.bot) return;
+  if (message.channel.type == "dm") return;
+  if (!message.content.startsWith(prefix)) return;
+  if (cmd) {
+    cmd.run(bot, message, args, Discord)
+    console.log(`${message.author.username} used the ${message.content.split(" ")[0]} command.`)
+  }
+})
+
+bot.login(process.env.token)
